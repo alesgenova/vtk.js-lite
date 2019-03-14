79af9993e8e780a0cc952e4c84bac6fd44e89c8a
Alessandro Genova <alessandro.genova@kitware.com>
2019-03-14 16:39:17 -0400

    Initial version

---
+++ src/index.js
@@ -0,0 +1,29 @@
+export { default as vtkActor } from 'vtk.js/Sources/Rendering/Core/Actor';
+export { default as vtkMapper } from 'vtk.js/Sources/Rendering/Core/Mapper';
+export { default as vtkSphereMapper } from 'vtk.js/Sources/Rendering/Core/SphereMapper';
+export { default as vtkStickMapper } from 'vtk.js/Sources/Rendering/Core/StickMapper';
+export { default as vtkColorTransferFunction } from 'vtk.js/Sources/Rendering/Core/ColorTransferFunction';
+export { default as vtkVolume } from 'vtk.js/Sources/Rendering/Core/Volume';
+export { default as vtkVolumeMapper } from 'vtk.js/Sources/Rendering/Core/VolumeMapper';
+
+export { default as vtkFullScreenRenderWindow } from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
+export { default as vtkGenericRenderWindow } from 'vtk.js/Sources/Rendering/Misc/GenericRenderWindow';
+
+export { default as vtkDataArray } from 'vtk.js/Sources/Common/Core/DataArray';
+export { default as vtkCellArray } from 'vtk.js/Sources/Common/Core/CellArray';
+export { default as vtkScalarsToColors } from 'vtk.js/Sources/Common/Core/ScalarsToColors';
+export { default as vtkLookupTable } from 'vtk.js/Sources/Common/Core/LookupTable';
+
+export { default as vtkPolyData } from 'vtk.js/Sources/Common/DataModel/PolyData';
+export { default as vtkMolecule } from 'vtk.js/Sources/Common/DataModel/Molecule';
+export { default as vtkImageData } from 'vtk.js/Sources/Common/DataModel/ImageData';
+export { default as vtkPiecewiseFunction } from 'vtk.js/Sources/Common/DataModel/PiecewiseFunction';
+
+export { default as vtkImageMarchingCubes } from 'vtk.js/Sources/Filters/General/ImageMarchingCubes';
+export { default as vtkMoleculeToRepresentation } from 'vtk.js/Sources/Filters/General/MoleculeToRepresentation';
+
+export { default as vtkPlaneSource } from 'vtk.js/Sources/Filters/Sources/PlaneSource';
+
+export { AttributeTypes } from 'vtk.js/Sources/Common/DataModel/DataSetAttributes/Constants';
+export { FieldDataTypes } from 'vtk.js/Sources/Common/DataModel/DataSet/Constants';
+export { Representation } from 'vtk.js/Sources/Rendering/Core/Property/Constants';
